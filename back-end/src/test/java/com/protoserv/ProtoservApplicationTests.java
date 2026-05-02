81237d27db703cb3257e2cdc09b3adb2b720f899
imbassado <edualansatos@gmail.com>
2026-05-01 22:31:31 -0300

    chore(setup): insere pasta de back-end, configura dependências e estrutura de pastas base
    - Adiciona dependências do Spring Security, JWT, JPA, Flyway, Spring Web, Lombok e MySQL Driver no pom.xml
    - Configura application.properties com credênciais do MySQL e variável do JWT
    - Cria a estrutura inicial de pacotes (controller, service, security, etc.)

---
+++ back-end/src/test/java/com/protoserv/ProtoservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protoserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
