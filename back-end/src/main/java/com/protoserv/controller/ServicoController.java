1db2c304bcdbf3c446d221108ea3f2b5eed01e93
imbassado <edualansatos@gmail.com>
2026-05-30 22:54:16 -0300

    feat(servico): expõe endpoint de listagem de serviços com busca e paginação
    - Adiciona no ServicoController a exposição do endpoint GET /servicos que aceita parâmetros de busca e paginação.
    - O parametro de busca é opcional e filtra os serviços pelo nome, ignorando maiúsculas e minúsculas.
    - O parâmetro de paginação tem um tamanho padrão de 6 itens por página e ordena os resultados pelo nome do serviço.

--- back-end/src/main/java/com/protoserv/controller/ServicoController.java
+++ back-end/src/main/java/com/protoserv/controller/ServicoController.java
@@ -1,9 +1,14 @@
 package com.protoserv.controller;
 
 import com.protoserv.dto.request.DadosCriarServicoDTO;
+import com.protoserv.dto.response.DadosListagemServicoDTO;
 import com.protoserv.dto.response.DadosServicoDTO;
 import com.protoserv.service.ServicoService;
 import jakarta.validation.Valid;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -28,5 +33,15 @@ public class ServicoController {
         var uri = uriBuilder.path("/servicos/{id}").buildAndExpand(servicoCriado.id()).toUri();
 
         return ResponseEntity.created(uri).body(servicoCriado);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemServicoDTO>> listarServicos(
+            @RequestParam(required = false) String busca,
+            @PageableDefault(size = 6, sort = {"nome"}) Pageable paginacao) {
+        
+        var page = servicoService.listarServicos(busca, paginacao);
+        
+        return ResponseEntity.ok(page);
     }
 }
