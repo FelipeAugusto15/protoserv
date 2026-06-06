6e5e486357bb9eee5b7fc083d7971796cac1418a
imbassado <edualansatos@gmail.com>
2026-06-06 16:38:28 -0300

    refactor(solicitacao): herda de JpaSpecificationExecutor para permitir filtros dinâmicos
    - Adiciona suporte a filtros dinâmicos na listagem de solicitações, permitindo que os atendentes possam filtrar por diversos critérios além do status, como data de abertura, tipo de serviço, etc

--- back-end/src/main/java/com/protoserv/repository/SolicitacaoRepository.java
+++ back-end/src/main/java/com/protoserv/repository/SolicitacaoRepository.java
@@ -11,8 +11,9 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
+public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long>, JpaSpecificationExecutor<Solicitacao> {
     
     Optional<Solicitacao> findFirstByCidadaoAndServicoAndStatusNotInOrderByDataAberturaDesc(
             Usuario cidadao, 
