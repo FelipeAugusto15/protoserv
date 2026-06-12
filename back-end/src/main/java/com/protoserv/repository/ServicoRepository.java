ed59b213177be7c790447ff607b348a14a14933d
imbassado <edualansatos@gmail.com>
2026-06-11 21:29:54 -0300

    refactor: remove `@Repository` das interfaces de repositório
    - Elimina anotações redundantes
    - Mantém o reconhecimento automático pelo Spring Data JPA

--- back-end/src/main/java/com/protoserv/repository/ServicoRepository.java
+++ back-end/src/main/java/com/protoserv/repository/ServicoRepository.java
@@ -5,11 +5,9 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.protoserv.model.Servico;
 
-@Repository
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
     
     Optional<Servico> findByNome(String nome);
