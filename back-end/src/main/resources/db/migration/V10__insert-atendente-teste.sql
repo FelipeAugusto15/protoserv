a53c1ecc2a47bb70fcda4782fb1ee0e0330a9bba
V1ctorGbr <vg1771049@gmail.com>
2026-06-15 18:39:55 -0300

    db: insere atendente de teste

---
+++ back-end/src/main/resources/db/migration/V10__insert-atendente-teste.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuarios (nome, email, senha, perfil, status) VALUES (
+    'Atendente Teste',
+    'atendente@protoserv.com',
+    '$2a$12$dJQLXpj8Zom3FM8rHrWPi.7V3ssXvCmeI.U6.Lsuh4clwMcHiQcDy',
+    'ATENDENTE',
+    'ATIVO'
+);
